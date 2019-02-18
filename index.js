7f873ab2739a758e6ce776bf6937ae65bb1b4e74
victorocna <victorocnarescu@gmail.com>
2019-02-18 16:09:48 +0200

    feat: add args passed from the cli

--- index.js
+++ index.js
@@ -1,46 +1,31 @@
-var fs = require('fs');
+const fs = require('fs')
 const path = require('path')
 const Jimp = require('jimp')
+const walk = require('./lib/walk.js')
+const argv = require('yargs-parser')(process.argv.slice(2))
 
-/**
- * @see https://stackoverflow.com/a/5827895
- */
-var walk = function(dir, done) {
-  var results = [];
-  fs.readdir(dir, function(err, list) {
-    if (err) return done(err);
-    var pending = list.length;
-    if (!pending) return done(null, results);
-    list.forEach(function(file) {
-      file = path.resolve(dir, file);
-      fs.stat(file, function(err, stat) {
-        if (stat && stat.isDirectory()) {
-          walk(file, function(err, res) {
-            results = results.concat(res);
-            if (!--pending) done(null, results);
-          });
-        } else {
-          results.push(file);
-          if (!--pending) done(null, results);
-        }
-      });
-    });
-  });
-};
-
-// TODO: command line options to specify directory and quality
 const options = {
-  src: path.resolve('test'),
+  dir: path.resolve('images'),
+  dest: path.resolve('build'),
   quality: 75,
+  ...argv,
+}
+options.dir = path.resolve(__dirname, options.dir)
+
+try {
+  fs.lstatSync(options.dir)
+} catch (err) {
+  console.error(`Error: no such file or directory ${options.dir}`)
+  process.exit(1)
 }
 
-walk(options.src, (err, files) => {
+walk(options.dir, (err, files) => {
   if (err) throw err;
 
   let items = 0
   const images = files.filter(isImage)
   images.map(async image => {
-    await optimize(image)
+    await optimize(image, options.quality, path.join(options.dest, image.substring(__dirname.length - 1)))
 
     items++
     if (images.length > 0 && items === images.length) {
@@ -49,12 +34,12 @@ walk(options.src, (err, files) => {
   })
 })
 
-const optimize = async (image) => {
+const optimize = async (image, quality, destination) => {
   return Jimp.read(image)
-    .then(lenna => {
-      return lenna
-        .quality(options.quality)
-        .write(path.join('build', image.substring(__dirname.length)))
+    .then(optimized => {
+      return optimized
+        .quality(quality)
+        .writeAsync(destination)
     })
     .catch(err => {
       console.error(err)
@@ -63,3 +48,10 @@ const optimize = async (image) => {
 
 const done = () => console.log('👍')
 const isImage = (file) => new RegExp('.+(jpe?g|png|gif)', 'gi').test(file)
+
+const imageSize = (image, cb) => {
+  fs.stat(image, (err, stats) => {
+    if (err) return cb(err)
+    cb(null, stats.size)
+  })
+}
