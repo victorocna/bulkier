a3c57faba00b728388adaa91382d7bba6628dd9c
victorocna <victorocnarescu@gmail.com>
2020-04-05 22:30:26 +0300

    update prettier options

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
-const fs = require("fs")
-const path = require("path")
-const Jimp = require("jimp")
-const compare = require("./lib/compare.js")
-const walk = require("./lib/walk.js")
-const utils = require("./lib/utils.js")
-const argv = require("yargs-parser")(process.argv.slice(2))
+const fs = require('fs');
+const path = require('path');
+const Jimp = require('jimp');
+const compare = require('./lib/compare.js');
+const walk = require('./lib/walk.js');
+const utils = require('./lib/utils.js');
+const argv = require('yargs-parser')(process.argv.slice(2));
 
 const options = {
-  dir: path.resolve("images"),
-  dest: path.resolve("build"),
+  dir: path.resolve('images'),
+  dest: path.resolve('build'),
   quality: 75,
   ...argv,
-}
-options.dir = path.resolve(__dirname, options.dir)
+};
+options.dir = path.resolve(__dirname, options.dir);
 
 try {
-  fs.lstatSync(options.dir)
+  fs.lstatSync(options.dir);
 } catch (err) {
-  console.error(`Error: no such file or directory ${options.dir}`)
-  process.exit(1)
+  console.error(`Error: no such file or directory ${options.dir}`);
+  process.exit(1);
 }
 
 walk(options.dir, (err, files) => {
-  if (err) throw err
+  if (err) throw err;
 
-  const images = files.filter(utils.isImage)
+  const images = files.filter(utils.isImage);
   images.map(async (image) => {
-    const optimized = path.join(options.dest, image.substring(__dirname.length - 1))
-    await optimize(image, options.quality, optimized)
+    const optimized = path.join(options.dest, image.substring(__dirname.length - 1));
+    await optimize(image, options.quality, optimized);
 
     if (await compare.isWorse(image, optimized)) {
       fs.copyFile(image, optimized, (err) => {
-        if (err) throw err
-      })
+        if (err) throw err;
+      });
     }
-  })
-})
+  });
+});
 
 const optimize = async (imagePath, quality, destination) => {
   return Jimp.read(imagePath)
     .then((image) => {
-      utils.markProgress()
+      utils.markProgress();
 
-      return image.quality(quality).writeAsync(destination)
+      return image.quality(quality).writeAsync(destination);
     })
     .catch((err) => {
-      console.error(err)
-    })
-}
+      console.error(err);
+    });
+};
--- resize.js
+++ resize.js
@@ -1,73 +1,73 @@
-const fs = require("fs")
-const path = require("path")
-const Jimp = require("jimp")
-const walk = require("./lib/walk.js")
-const utils = require("./lib/utils.js")
-const argv = require("yargs-parser")(process.argv.slice(2))
+const fs = require('fs');
+const path = require('path');
+const Jimp = require('jimp');
+const walk = require('./lib/walk.js');
+const utils = require('./lib/utils.js');
+const argv = require('yargs-parser')(process.argv.slice(2));
 
 const options = {
-  dir: path.resolve("images"),
-  dest: path.resolve("build"),
+  dir: path.resolve('images'),
+  dest: path.resolve('build'),
   skipSmaller: false,
   quality: 75,
   ...argv,
-}
-options.dir = path.resolve(__dirname, options.dir)
+};
+options.dir = path.resolve(__dirname, options.dir);
 
 try {
-  fs.lstatSync(options.dir)
+  fs.lstatSync(options.dir);
 } catch (err) {
-  console.error(`Error: no such file or directory ${options.dir}`)
-  process.exit(1)
+  console.error(`Error: no such file or directory ${options.dir}`);
+  process.exit(1);
 }
 
 walk(options.dir, (err, files) => {
   if (err) {
-    throw err
+    throw err;
   }
 
   const breakpoints = {
-    576: "small",
-    768: "medium",
-    992: "large",
-    1200: "xlarge",
-    1920: "full-hd",
-    3840: "ultra-4k",
-  }
+    576: 'small',
+    768: 'medium',
+    992: 'large',
+    1200: 'xlarge',
+    1920: 'full-hd',
+    3840: 'ultra-4k',
+  };
 
-  const images = files.filter(utils.isImage)
+  const images = files.filter(utils.isImage);
   images.map(async (image) => {
-    options.path = path.join(options.dest, image.substring(__dirname.length - 1))
-    options.breakpoints = breakpoints
+    options.path = path.join(options.dest, image.substring(__dirname.length - 1));
+    options.breakpoints = breakpoints;
 
-    Object.keys(breakpoints).map(async (width) => await resize(image, +width, options))
-  })
-})
+    Object.keys(breakpoints).map(async (width) => await resize(image, +width, options));
+  });
+});
 
 const resize = async (imagePath, width, options) => {
-  const writePath = options.path
+  const writePath = options.path;
   return Jimp.read(imagePath)
     .then((image) => {
       if (image.getWidth() <= width) {
         if (options.skipSmaller) {
-          return
+          return;
         }
-        width = image.getWidth()
+        width = image.getWidth();
       }
 
-      let suffix = width
+      let suffix = width;
       if (options.breakpoints && options.breakpoints[width]) {
-        suffix = options.breakpoints[width]
+        suffix = options.breakpoints[width];
       }
 
-      utils.markProgress()
+      utils.markProgress();
       return image
         .resize(width, Jimp.AUTO)
         .quality(options.quality)
-        .writeAsync(utils.uniquePath(writePath, suffix))
+        .writeAsync(utils.uniquePath(writePath, suffix));
     })
     .catch((err) => {
-      console.error(err)
-      process.exit(1)
-    })
-}
+      console.error(err);
+      process.exit(1);
+    });
+};
